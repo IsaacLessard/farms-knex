c334d4e41271f7f84751530c1be3b93174d39187
Roberto Ortega <berto.ort@gmail.com>
2015-12-16 15:58:33 -0700

    🐏

--- routes/farms.js
+++ routes/farms.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
-/* GET home page. */
+function Farms() {
+  return knex('farms')
+}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Farms().select().then(function (farms) {
+    res.json(farms);
+  }).catch(function (err) {
+    res.json(err);
+  });
 });
+
+
+router.post('/', function (req, res, next) {
+  Farms().insert({
+    name: req.body.name,
+  }, 'id')
+  .then(function (farmid) {
+    res.json(farmid)
+  }).catch(function (err) {
+    res.json(err);
+  });
+})
+
 
 module.exports = router;
